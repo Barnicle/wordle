07a9e03f6b6ebb043e5908c2af5ed7fd5a1edd43
Evgenii Galimov <egalimov95@gmail.com>
2022-03-19 15:31:41 +0300

    minor fix

--- src/word-utiles.test.ts
+++ src/word-utiles.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest'
 import { getRandomWord } from './word-utils'
 
-describe('Testin word-utils', () => {
+describe('Testing word-utils', () => {
   it('random word', () => {
     expect(getRandomWord()).toBeTruthy();
     expect(getRandomWord().length).toEqual(5)
