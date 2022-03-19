66883cfca297df1d7c97458a87da2ecab58997d1
Evgenii Galimov <egalimov95@gmail.com>
2022-03-19 19:31:23 +0300

    add words algorithm

--- src/word-utils.ts
+++ src/word-utils.ts
@@ -4,3 +4,77 @@ export const getRandomWord = () => {
   const randomIndex = Math.floor(Math.random() * wordBank.length)
   return wordBank[randomIndex]
 }
+
+export enum LetterState {
+  Miss = 'Miss',
+  Present = 'Present',
+  Match = 'Match'
+}
+interface ComputeGuessPros {
+  guess: string
+  answer: string
+}
+
+export function computeGuess(
+  guess: string,
+  answerString: string
+): LetterState[] {
+  const result: LetterState[] = [];
+
+  if (guess.length !== answerString.length) {
+    return result;
+  }
+
+  const answer = answerString.split('');
+
+  const guessAsArray = guess.split('');
+
+  const answerLetterCount: Record<string, number> = {};
+
+  // alternative approaches to this logic
+  // https://github.com/rauchg/wordledge/blob/main/pages/_middleware.ts#L46-L69
+
+  guessAsArray.forEach((letter, index) => {
+    const currentAnswerLetter = answer[index];
+
+    answerLetterCount[currentAnswerLetter] = answerLetterCount[
+      currentAnswerLetter
+    ]
+      ? answerLetterCount[currentAnswerLetter] + 1
+      : 1;
+
+    if (currentAnswerLetter === letter) {
+      result.push(LetterState.Match);
+    } else if (answer.includes(letter)) {
+      result.push(LetterState.Present);
+    } else {
+      result.push(LetterState.Miss);
+    }
+  });
+
+  result.forEach((curResult, resultIndex) => {
+    if (curResult !== LetterState.Present) {
+      return;
+    }
+
+    const guessLetter = guessAsArray[resultIndex];
+
+    answer.forEach((currentAnswerLetter, answerIndex) => {
+      if (currentAnswerLetter !== guessLetter) {
+        return;
+      }
+
+      if (result[answerIndex] === LetterState.Match) {
+        result[resultIndex] = LetterState.Miss;
+      }
+
+      if (answerLetterCount[guessLetter] <= 0) {
+        result[resultIndex] = LetterState.Miss;
+      }
+    });
+
+    answerLetterCount[guessLetter]--;
+  });
+
+  return result;
+}
